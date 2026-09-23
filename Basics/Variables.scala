e79ee39e715310b23e1eb27262fe793ab9d34008
Neha1016 <nehachouhan1016@gmail.com>
2026-09-23 19:29:49 +0530

    ADD

---
+++ Basics/Variables.scala
@@ -0,0 +1,13 @@
+object Main {
+
+    def main(args: Array[String]): Unit = {
+        val name = "Shiv"
+        var age = 22
+        val course = "MBA"
+
+        println(name)
+        println(age)
+        println(course)
+    }
+
+}
---
+++ College/info.scala
@@ -0,0 +1,7 @@
+object Main {
+
+    def main(args: Array[String]): Unit = {
+        println("My name is Nutan\nI am Learning Scala\nI am a B.Tech AI Student")
+    }
+
+}
