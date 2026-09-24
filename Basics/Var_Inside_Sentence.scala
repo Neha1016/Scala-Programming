1ae3d6d390e36dd7a877a053992295acd44fda93
Neha1016 <nehachouhan1016@gmail.com>
2026-09-24 21:28:28 +0530

    Add Scala programs

---
+++ Basics/Var_Inside_Sentence.scala
@@ -0,0 +1,14 @@
+object main {
+
+    def main(args:Array[String]) : Unit = {
+
+        val name = "Shiv"
+        val age = 25
+        val course = "B.Tech"
+
+        println(s"My name is $name")
+        println(s"I am $age is old")
+        println(s"I am studying $course")
+
+    }
+}
