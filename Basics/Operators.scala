5f14bbf6cb6f68b75c3a97c46a13cd6cb3ca069d
Neha1016 <nehachouhan1016@gmail.com>
2026-09-26 22:07:15 +0530

    ADD

---
+++ Basics/Operators.scala
@@ -0,0 +1,13 @@
+object Main {
+    def main(args:Array[String]) : Unit = {
+
+        val a = 22
+        val b = 12
+
+        println(s"Addition : ${a + b} ")
+        println(s"Subtraction : ${a - b} ")
+        println(s"Multiplication : ${a *b} ")
+        println(s"Division : ${a / b} ")
+        println(s"Remainder: ${a % b} ")
+    }
+}
